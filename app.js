ea9c3e10db4973ae674e805ca4158a414d120305
MDesu <2dpunk3@mail.ru>
2024-03-23 21:27:11 +0800

    Комменты

--- app.js
+++ app.js
@@ -28,26 +28,20 @@ function sendTransaction() {
 // Call updateBalance when the page loads
 updateBalance();
 
-
-
-
-// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-// # # Маршрут для получения баланса пользователя
-// # @app.route('/get_balance', methods=['GET'])
-// # def get_balance():
-// #     # Здесь должна быть логика получения баланса пользователя из блокчейна
-// #     # Возвратим для примера статический баланс 100
-// #     return jsonify({'balance': 100})
-
-// # # Маршрут для отправки транзакции
-// # @app.route('/send_transaction', methods=['POST'])
-// # def send_transaction():
-// #     data = request.get_json()
-// #     recipient = data['recipient']
-// #     amount = data['amount']
-// #     # Здесь должна быть логика отправки транзакции в блокчейн
-// #     # Добавим транзакцию в текущий блок
-// #     blockchain.new_transaction(sender="current_user", recipient=recipient, amount=amount)
-// #     # Возвращаем сообщение об успешной транзакции
-// #     return jsonify({'message': 'Transaction sent successfully'})
-// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+//Маршрут для получения баланса пользователя
+//@app.route('/get_balance', methods=['GET'])
+//def get_balance():
+//     # Здесь должна быть логика получения баланса пользователя из блокчейна
+//     # Возвратим для примера статический баланс 100
+//     return jsonify({'balance': 100})
+// Маршрут для отправки транзакции
+// @app.route('/send_transaction', methods=['POST'])
+// def send_transaction():
+//     data = request.get_json()
+//     recipient = data['recipient']
+//     amount = data['amount']
+//     # Здесь должна быть логика отправки транзакции в блокчейн
+//     # Добавим транзакцию в текущий блок
+//     blockchain.new_transaction(sender="current_user", recipient=recipient, amount=amount)
+//     # Возвращаем сообщение об успешной транзакции
+//    return jsonify({'message': 'Transaction sent successfully'})
